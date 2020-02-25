3d077e0dcb23caaf72890ac3f849afcd4a818e7c
conanlu <53196170+conanlu@users.noreply.github.com>
2020-02-25 01:07:15 -0800

    hi just uploading my gamecontrol stuff!

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormApp1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        private GameControl myGC;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int v = myGC.getGameState();
+            label1.Text = v.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myGC = new GameControl();
+        }
+    }
+}
---
+++ WindowsFormsApp1/GameControl.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormApp1
+{
+    class GameControl
+    {
+        private int gameState;
+        private Boolean displayMainMenu;
+        private Boolean displayHighScorePage;
+        private Boolean displayInstructions;
+        private Boolean launchGame;
+
+
+        public GameControl()
+        {
+
+        }
+
+        public int getGameState()
+        {
+            //accessor for the game state: planning on 0: entry screen, 1: in game,
+            //2: viewing high scores, or something like that
+            return gameState;
+        }
+
+        public void setGameState(int gameState)
+        {
+            //mutator for game state variable
+            this.gameState = gameState;
+        }
+
+        public void mainMenuActivated()
+        {
+            displayMainMenu = true;
+            //passes onto UI, passes onto highscore, etc. if triggered
+        }
+
+        public void highScorePageActivated()
+        {
+            if (displayMainMenu)
+            {
+                displayHighScorePage = true;
+            }
+            //passes onto highscore
+        }
+
+        public void launchGameActivated()
+        {
+            if (displayMainMenu)
+            {
+                launchGame = true;
+                gameState = 1;
+            }
+            //passes onto game creation: player, location, UI, etc.
+        }
+        public void instructionsActivated()
+        {
+            if (displayMainMenu)
+            {
+                displayInstructions = true;
+            }
+            //passes onto instructions page (UI)
+        }
+
+
+
+    }
+}
