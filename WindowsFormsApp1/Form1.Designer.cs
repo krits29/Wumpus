202f6f133ab60f69f5eba35ae27e63653dc7f845
conanlu <53196170+conanlu@users.noreply.github.com>
2020-02-29 20:44:41 -0800

    another namespace change whoops it's Wumpus

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp1
+﻿namespace Wumpus
 {
     partial class Form1
     {
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormApp1;
 
-namespace WindowsFormsApp1
+namespace Wumpus
 {
     public partial class Form1 : Form
     {
