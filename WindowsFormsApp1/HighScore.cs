3111c0a62253b5e3aa4819036fc365659812a14e
shradha504 <60935542+shradha504@users.noreply.github.com>
2020-03-04 00:13:27 -0800

    Update HighScore.cs

--- WindowsFormsApp1/HighScore.cs
+++ WindowsFormsApp1/HighScore.cs
@@ -1,16 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Wumpus
+namespace StubbedClass
 {
     class HighScore
     {
-        public HighScore()
+        private String _fileName;
+        public HighScore(String fileName)
         {
-            // Constructor
+            _fileName = fileName;
         }
+           
+        public void setHighScore(String playerName, int score, String caveName)
+        {
+            //purpose is to set the new highscore affter 
+        }
+
+        //sally 334 random1
+        public String[] getHighScores()
+        {
+            return null;
+            //purpose is to return the highscore chart with updated values 
+        }
+
     }
 }
+
